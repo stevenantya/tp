699d68c8633917d7e66f88e024240c6595e8a661
LAPTOP-DQSB43T9\deepa <deepanjali.dhawan@gmail.com>
2023-03-24 00:18:56 +0800

    Update 1. v2 version List 2. Parser Class 3. Ran JUnit Tests

--- src/main/java/seedu/duke/command/ListCommand.java
+++ src/main/java/seedu/duke/command/ListCommand.java
@@ -4,6 +4,14 @@ import seedu.duke.exceptions.secrets.NonExistentFolderException;
 import seedu.duke.secrets.Secret;
 import seedu.duke.storage.SecretMaster;
 
+import seedu.duke.secrets.BasicPassword;
+import seedu.duke.secrets.CreditCard;
+import seedu.duke.secrets.CryptoWallet;
+import seedu.duke.secrets.NUSNet;
+import seedu.duke.secrets.StudentID;
+import seedu.duke.secrets.WifiPassword;
+
+
 import java.util.ArrayList;
 
 public class ListCommand extends Command {
@@ -27,6 +35,63 @@ public class ListCommand extends Command {
         return false;
     }
 
+    private String getSecretTypeInfo(Secret secret) {
+        if (secret instanceof BasicPassword) {
+            BasicPassword basicPassword = (BasicPassword) secret;
+            return
+                    "Type of Secret: Basic Password" + "\n" +
+                    "Name: " + basicPassword.getName() + "\n" +
+                    "Folder: " + basicPassword.getFolderName() + "\n" +
+                    "Username: " + basicPassword.getUsername() + "\n" +
+                    "URL: " + basicPassword.getUrl() + "\n" +
+                    "Password: " + basicPassword.getPassword();
+
+        } else if (secret instanceof CreditCard) {
+            CreditCard creditCard = (CreditCard) secret;
+            return "Type of Secret: Credit Card" + "\n" +
+                    "Name: " + creditCard.getName() + "\n" +
+                    "Folder: " + creditCard.getFolderName() + "\n" +
+                    "Full Name: " + creditCard.getFullName() + "\n" +
+                    "Credit Card Number: " + creditCard.getCreditCardNumber() + "\n" +
+                    "CVC Number: " + creditCard.getCvcNumber() + "\n" +
+                    "Expiry Date: " + creditCard.getExpiryDate();
+
+        } else if (secret instanceof CryptoWallet) {
+            CryptoWallet cryptoWallet = (CryptoWallet) secret;
+            return "Type of Secret: CryptoCurrency Wallet" + "\n" +
+                    "Name: " + cryptoWallet.getName() + "\n" +
+                    "Folder: " + cryptoWallet.getFolderName() + "\n" +
+                    "Username: " + cryptoWallet.getUsername() + "\n" +
+                    "Private Key: " + cryptoWallet.getPrivateKey() + "\n" +
+                    "Seed Phrase: " + cryptoWallet.getSeedPhrase();
+
+        } else if (secret instanceof NUSNet) {
+            NUSNet NUSnet = (NUSNet) secret;
+            return "Type of Secret: NUSNet ID" + "\n" +
+                    "Name: " + NUSnet.getName() + "\n" +
+                    "Folder: " + NUSnet.getFolderName() + "\n" +
+                    "NUSNet ID: " + NUSnet.getnusNetId() + "\n" +
+                    "Password: " + NUSnet.getPassword();
+
+        } else if (secret instanceof StudentID) {
+            StudentID studentID = (StudentID) secret;
+            return "Type of Secret: Student ID" + "\n" +
+                    "Name: " + studentID.getName() + "\n" +
+                    "Folder: " + studentID.getFolderName() + "\n" +
+                    "Student ID: " + studentID.getStudentID();
+
+        } else if (secret instanceof WifiPassword) {
+            WifiPassword wifiPassword = (WifiPassword) secret;
+            return "Type of Secret: Wifi Password" + "\n" +
+                    "Name: " + wifiPassword.getName() + "\n" +
+                    "Folder: " + wifiPassword.getFolderName() + "\n" +
+                    "Username: " + wifiPassword.getUsername() + "\n" +
+                    "Password: " + wifiPassword.getPassword();
+
+        } else {
+            return "Secret";
+        }
+    }
     @Override
     public void execute(SecretMaster secureNUSData) {
         ArrayList<Secret> secrets;
@@ -41,8 +106,11 @@ public class ListCommand extends Command {
                 return;
             }
             System.out.println("List of secrets:");
+            int counter = 1;
             for (Secret secret : secrets) {
-                System.out.println(secret.getName());
+                String secretTypeInfo = getSecretTypeInfo(secret);
+                System.out.println(counter + ". " + secretTypeInfo);
+                counter++;
             }
         } catch (NonExistentFolderException e) {
             System.out.println("Folder " + folderName + " does not exist.");
--- src/test/java/seedu/duke/command/ListCommandTest.java
+++ src/test/java/seedu/duke/command/ListCommandTest.java
@@ -1,5 +1,23 @@
 package seedu.duke.command;
 
+import seedu.duke.exceptions.secrets.FolderExistsException;
+import seedu.duke.exceptions.secrets.IllegalFolderNameException;
+import seedu.duke.exceptions.secrets.IllegalSecretNameException;
+import seedu.duke.exceptions.secrets.InvalidURLException;
+import seedu.duke.exceptions.secrets.InvalidCreditCardNumberException;
+import seedu.duke.exceptions.secrets.InvalidExpiryDateException;
+import seedu.duke.exceptions.RepeatedIdException;
+
+
+import seedu.duke.secrets.BasicPassword;
+import seedu.duke.secrets.CreditCard;
+import seedu.duke.secrets.CryptoWallet;
+import seedu.duke.secrets.NUSNet;
+import seedu.duke.secrets.StudentID;
+import seedu.duke.secrets.WifiPassword;
+
+import seedu.duke.storage.SecretMaster;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -27,5 +45,26 @@ public class ListCommandTest {
     void getList_withFolder() {
         ListCommand listCommand = new ListCommand("list f/Folder123!");
         assertEquals(listCommand.extractFolderName("list f/Folder123!"), "f/Folder123!");
+    }
+
+    @Test
+    void testSecretTypes() {
+        SecretMaster secretMaster = new SecretMaster();
+
+        try {
+            secretMaster.addSecret(new BasicPassword("BasicPassword1", "Username1", "Password1", "Url1.com"));
+            secretMaster.addSecret(new CreditCard("CreditCard1", "John Doe", "1234567812345678", 123, "12/25"));
+            secretMaster.addSecret(new CryptoWallet("CryptoWallet1", "Folder1", "DeepsD", "PrivateKey1", "SeedPhrase1"));
+            secretMaster.addSecret(new NUSNet("NUSNet1", "NUSNetId1", "Password1"));
+            secretMaster.addSecret(new StudentID("StudentID1", "StudentID1"));
+            secretMaster.addSecret(new WifiPassword("WifiPassword1", "Username1", "Password1"));
+
+            ListCommand listCommand = new ListCommand("list");
+            listCommand.execute(secretMaster);
+
+        } catch (FolderExistsException | IllegalFolderNameException | IllegalSecretNameException | RepeatedIdException
+                | InvalidURLException | InvalidCreditCardNumberException | InvalidExpiryDateException e) {
+            e.printStackTrace();
+        }
     }
 }
