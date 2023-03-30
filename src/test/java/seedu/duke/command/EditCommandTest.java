ac7773dca992c7811987389041e3512480e180ce
ollayf <62301945+ollayf@users.noreply.github.com>
2023-03-30 23:21:00 +0800

    Update EditCommandTest.java

--- src/test/java/seedu/duke/command/EditCommandTest.java
+++ src/test/java/seedu/duke/command/EditCommandTest.java
@@ -16,7 +16,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertNull;
+// import static org.junit.jupiter.api.Assertions.assertNull;
 
 /**
  * The EditCommandTest class tests the EditCommand class.
