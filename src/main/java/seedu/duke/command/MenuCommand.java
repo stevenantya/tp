6e36123a9441bd889b57eb0d2c51020237853534
ollayf <62301945+ollayf@users.noreply.github.com>
2023-04-06 10:07:27 +0800

    Updated the menu and starting commands

--- src/main/java/seedu/duke/command/MenuCommand.java
+++ src/main/java/seedu/duke/command/MenuCommand.java
@@ -1,6 +1,7 @@
 package seedu.duke.command;
 
 import seedu.duke.storage.SecretMaster;
+import seedu.duke.ui.Ui;
 
 /**
  * Class that displays the list of all available commands in a menu format.
@@ -11,28 +12,26 @@ public class MenuCommand extends Command {
      * Displays the list of all available commands in a menu format.
      */
     public void displayMenu() {
-        System.out.println("SecureNUS Command Menu:\n" +
-                "1. Add a general password: new [PASSWORD_NAME] /f [FOLDER_NAME]\n" +
-                "2. Add password for websites needing Credit Card details: " +
-                "new o/CreditCard [NAME] /f [FOLDER_NAME]\n" +
-                "3. Add password for websites needing Crypto Wallet details: " +
-                "new o/CryptoWallet [NAME] /f [FOLDER_NAME]\n" +
-                "4. Add password for websites needing NUSNet ID: " +
-                "new o/NUSNet [NAME] /f [FOLDER_NAME]\n" +
-                "5. Add passwords for websites needing NUS StudentID (starting with A): " +
-                "new o/StudentID [NAME] /f [FOLDER_NAME]\n" +
-                "6. Add passwords for websites needing Wifi Passwords: " +
-                "new o/WifiPassword [NAME] /f [FOLDER_NAME]\n" +
-                "7. Delete the unwanted password: delete\n" +
-                "8. List all the passwords: list [f/FOLDER_NAME]\n" +
-                "9. View password: view p/PASSWORD_NAME \n" +
-                "10. Search for password: search n/NAME_LIKE [-f f/FOLDER_LIKE]\n" +
-                "11. Save the password in .txt file: save\n" +
-                "12. Load the password from the .txt file: load [-a, –append] [f/FILE_PATH]\n" +
-                "(BUGGY, NOT COMPLETED) 13. Edit the passwords: edit p/PASSWORD_NAME [-f nf/NEW_FOLDER_NAME]\n " +
-                "[-d nd/NEW_DESCRIPTION] [-N np/NEW_PASSWORD_NAME]\n" +
-                "14. Menu list to refer for commands: menu\n" +
-                "15. Exit from the password manager: exit\n"
+        Ui.inform("SecureNUS Command Menu:\n" +
+                "1. Add a general password: new [PASSWORD_NAME] [f/FOLDER_NAME]\n" +
+                "\tAdd Credit Card details: " +
+                "new o/CreditCard [NAME] [f/FOLDER_NAME]\n" +
+                "\tAdd details for a Crypto Wallet: " +
+                "new o/Crypto [NAME] [f/FOLDER_NAME]\n" +
+                "\tAdd a NUSNet ID and Password: " +
+                "new o/NUSNet [NAME] [f/FOLDER_NAME]\n" +
+                "\tAdd a NUS StudentID details (starting with A and ends with capital Alphabet): " +
+                "new o/StudentID [NAME] [f/FOLDER_NAME]\n" +
+                "\tAdd a password for Wifi: " +
+                "new o/WifiPassword [NAME] [f/FOLDER_NAME]\n" +
+                "2. Delete the unwanted secrets: delete\n" +
+                "3. List all the secrets: list [f/FOLDER_NAME]\n" +
+                "4. View secrets: view PASSWORD_NAME\n" +
+                "5. Search for secrets: search n/NAME_LIKE [f/FOLDER]\n" +
+                "6. Export the secrets in .txt file: save\n" +
+                "7. Edit a secrets: edit PASSWORD_NAME\n" +
+                "8. Menu list to refer for commands: menu\n" +
+                "9. Exit from the password manager (will automatically save this session): exit"
         );
     }
 
